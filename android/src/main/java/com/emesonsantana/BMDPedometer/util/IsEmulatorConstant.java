e7dab33408ed6f85d75c744045ab55825e14abda
Aaron Boone <aboone@fusionlabs.net>
2021-01-07 17:24:51 -0600

    Add missing class

---
+++ android/src/main/java/com/emesonsantana/BMDPedometer/util/IsEmulatorConstant.java
@@ -0,0 +1,33 @@
+package com.emesonsantana.BMDPedometer.util;
+
+import android.os.Build;
+import android.os.Bundle;
+
+public class IsEmulatorConstant {
+
+    // could be wrong, but trying, also this is not expected to change after app install
+    public static boolean isEmulator;
+
+    static {
+        isEmulator = testEmulator();
+    }
+
+    private static boolean testEmulator() {
+        return (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.HARDWARE.contains("goldfish")
+                || Build.HARDWARE.contains("ranchu")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || Build.PRODUCT.contains("sdk_google")
+                || Build.PRODUCT.contains("google_sdk")
+                || Build.PRODUCT.contains("sdk")
+                || Build.PRODUCT.contains("sdk_x86")
+                || Build.PRODUCT.contains("vbox86p")
+                || Build.PRODUCT.contains("emulator")
+                || Build.PRODUCT.contains("simulator");
+    }
+}
